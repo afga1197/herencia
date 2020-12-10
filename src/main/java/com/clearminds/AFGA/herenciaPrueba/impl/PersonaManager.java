8b0f01fa98d948088dd62a98bad027b71b6c1786
Andres Gomez <andrefelgoal@gmail.com>
2020-12-10 11:22:18 -0500

    Clase con Class forName

--- src/main/java/com/clearminds/AFGA/herenciaPrueba/impl/PersonaManager.java
+++ src/main/java/com/clearminds/AFGA/herenciaPrueba/impl/PersonaManager.java
@@ -7,8 +7,15 @@ public class PersonaManager {
 	
 	ServicioPersona serv;
 
-	public PersonaManager() {
-		serv = new ServicioPersonaArchivos();
+	public PersonaManager() throws InstanceException {
+		Class<?> clase;
+		try {
+			clase = Class.forName("com.clearminds.AFGA.herenciaPrueba.impl.ServicioPersonaBDD");
+			serv= (ServicioPersona) clase.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		}
 	}
 	
 	public void insertarPersona(Persona persona){
--- src/main/java/com/clearminds/AFGA/herenciaPrueba/impl/TestManager.java
+++ src/main/java/com/clearminds/AFGA/herenciaPrueba/impl/TestManager.java
@@ -5,12 +5,17 @@ import com.clearminds.AFGA.herenciaPrueba.model.Persona;
 public class TestManager {
 
 	public static void main(String[] args) {
-		PersonaManager personaManager = new PersonaManager();
-		Persona persona = new Persona();
-		persona.setNombre("Andres");
-		persona.setApellido("Gomez");
-		persona.setEdad(20);
-		personaManager.insertarPersona(persona);;
+		PersonaManager personaManager;
+		try {
+			personaManager = new PersonaManager();
+			Persona persona = new Persona();
+			persona.setNombre("Andres");
+			persona.setApellido("Gomez");
+			persona.setEdad(20);
+			personaManager.insertarPersona(persona);
+		} catch (InstanceException e) {
+			e.printStackTrace();
+		}
 
 	}
 
